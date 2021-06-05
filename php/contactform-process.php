a664b70cadd54e929da0c9092158212bd8129a3c
SaiGanesh81 <gsai64732@gmail.com>
2021-06-05 10:16:45 +0530

    changes

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -25,8 +25,8 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Leno landing page";
+$EmailTo = "saigana124@gmail.com";
+$Subject = "New message from Barcode based Leave";
 
 // prepare email body text
 $Body = "";
